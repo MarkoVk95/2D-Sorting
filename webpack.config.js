a4319e277ac420b1c761b4334e24ac43e12334e6
MV <markovk09@gmail.com>
2019-10-05 01:36:21 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
