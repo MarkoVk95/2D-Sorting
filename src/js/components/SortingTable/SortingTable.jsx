72cf065c523d31b4f6887602d2f9d18571b19e4b
Marko V <marko.vk@outlook.com>
2019-10-06 13:31:30 +0300

    add svg to SortingList

--- src/js/components/SortingTable/SortingTable.jsx
+++ src/js/components/SortingTable/SortingTable.jsx
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class SortingTable extends Component {
     render() {
         return (
-            <div className="container text-center">
-                Sorting Table
+            <div className="container d-flex justify-content-center my-5">
+                <svg height="600px" width="1000px" className="bg-light">
+
+                </svg>
             </div>
         )
     }
